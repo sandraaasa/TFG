f4100f27af350d05f61244acc8e9ef1b9f9f17a0
sandra <sandra_saanchez@hotmail.com>
2023-04-24 21:45:03 +0200

    se ve algo TTTTTT

--- tusalacine/src/App.js
+++ tusalacine/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import React from 'react';
 import HeaderPrincipal from './Component/HeaderPrincipal';
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  
+import "primereact/resources/primereact.min.css";    
 /* import MainContent from './MainContent';
 import Footer from './Footer'; */
 
--- tusalacine/src/index.js
+++ tusalacine/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';   
+import 'primereact/resources/primereact.css';                       
+import 'primeicons/primeicons.css';                                 
+import 'primeflex/primeflex.css';                                   
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
