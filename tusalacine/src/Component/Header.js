7a3e1d9349db3d6363c7b73c1a3952a3aa4acd11
Sandra <100119920+sandraaasa@users.noreply.github.com>
2023-04-24 16:35:32 +0200

    hi

--- tusalacine/src/Component/Header.js
+++ tusalacine/src/Component/Header.js
@@ -1,6 +1,9 @@
-<nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+<nav >
+{/* class="navbar navbar-light bg-light" */}
+  <div>
+  {/* class="container-fluid" */}
+    <a  href="#">
+    {/* class="navbar-brand" */}
       {/* la imagen del logo o sirectamente sin logo y pongo la foto de la persona */}
       Tu Sala Cine
     </a>
