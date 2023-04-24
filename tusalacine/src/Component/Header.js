cb49c002f2c495f7b39c40765a237f40d8009463
Sandra <100119920+sandraaasa@users.noreply.github.com>
2023-04-24 15:47:07 +0200

    creando el header

---
+++ tusalacine/src/Component/Header.js
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      {/* la imagen del logo o sirectamente sin logo y pongo la foto de la persona */}
+      Tu Sala Cine
+    </a>
+  </div>
+</nav>
