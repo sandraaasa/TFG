8cab060da5902e1123e88d7926347baabc52d5c5
Sandra <100119920+sandraaasa@users.noreply.github.com>
2023-06-05 08:00:45 +0000

    Update vistas.js

--- TSC/back/src/controller/vistas.js
+++ TSC/back/src/controller/vistas.js
@@ -70,7 +70,7 @@ let controller = {
           pelisget.push(añadir);
         }
         return res.status(200).send({
-          pelisUsu,pelisget
+          pelisUsu,
         });
       }
     } catch (error) {
