ab46013820a161d2b40327af7cca57256dd5f841
Sandra <100119920+sandraaasa@users.noreply.github.com>
2023-06-06 11:59:09 +0000

    Update 2 files

--- TSC/tusalacine/src/Global.js
+++ TSC/tusalacine/src/Global.js
@@ -1,6 +1,6 @@
 var Global = {
-    //url: "http://localhost:3001/"
-    url: "https://70eb65-3001.csb.app/"
-    //url: "https://backtsc.onrender.com/"
-}
+  //url: "http://localhost:3001/"
+  url: "https://i6xuln-3001.csb.app/",
+  //url: "https://backtsc.onrender.com/"
+};
 export default Global;
